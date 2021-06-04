9f524fd3f6f4b858b5662236739c71a8da7d18eb
amitle2 <73496384+amitle2@users.noreply.github.com>
2021-06-04 19:50:36 +0300

    Update fa.v

--- fa.v
+++ fa.v
@@ -1,18 +1,12 @@
 `timescale 1ns/100ps
 
-module full_adder(S, Cout, A, B, Cin);
-   input  A;
-   input  B;
-   input  Cin;
-   output S;
-   output Cout;
-   
-   wire   w1,w2,w3,w4;
 
-   xor(w1, A, B);
-   xor(S, Cin, w1);
-   and(w2, A, B);   
-   and(w3, A, Cin);
-   and(w4, B, Cin);   
-   or(Cout, w2, w3, w4);
+module full_adder(A, B, Cin, S, Cout);
+
+ output S, Cout;
+ input A, B, Cin;
+
+ assign S = A ^ B ^ Cin; 
+ assign Cout = (A & B) | Cin & (A ^ B);
+
 endmodule
