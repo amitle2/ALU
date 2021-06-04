c7560dfcbe4b531c4acbc6e2c3b8242250c2c8f5
amitle2 <73496384+amitle2@users.noreply.github.com>
2021-06-04 19:45:08 +0300

    Update fba.v

--- fba.v
+++ fba.v
@@ -1,28 +1,27 @@
-  `timescale 1ns/100ps
+`timescale 1ns/100ps
 
-module FourBitAddSub(S, C, V, A, B, Op);
-   input [3:0] 	A;
-   input [3:0] 	B;
-   input 	Op;  // 0 = A-B ; 1 = A-B
-   output [3:0] S;   //Result
-   output 	C;   //carry bit
-   output 	V;   //overflow bit
+module FourBitAddSub(A, B, M, S, C,V);
 
-   
-   wire 	C0,C1,C2,C3; // The carry out wires
-   wire 	xor0,xor1,xor2,xor3; // B xor Op
-    
-   xor(xor0, B[0], Op);
-   xor(xor1, B[1], Op);
-   xor(xor2, B[2], Op);
-   xor(xor3, B[3], Op);
+ input [3:0] A, B;
+ input M;              // 0 = A-B ; 1 = A-B
+ output [3:0] S;       //Result
+ output C,V;           //C=carry bit, V=overflow bit
 
-   xor(C, C3, Op);
-   xor(V, C3, C2);
-   
-   full_adder fa0(S[0], C0, A[0], xor0, Op);
-   full_adder fa1(S[1], C1, A[1], xor1, C0);
-   full_adder fa2(S[2], C2, A[2], xor2, C1);
-   full_adder fa3(S[3], C3, A[3], xor3, C2);
+ wire C1,C2,C3;        // The carry out wires
+ wire B0,B1,B2,B3,M0;  // B xor M
+
+ assign M0 = ~M;
+
+ xor(B0, B[0], M0);
+ xor(B1, B[1], M0);
+ xor(B2, B[2], M0);
+ xor(B3, B[3], M0);
+ xor(V, C3,C2);
+
+ full_adder fa0(A[0], B0, M0, S[0], C1);
+ full_adder fa1(A[1], B1, C1, S[1], C2);
+ full_adder fa2(A[2], B2, C2, S[2], C3);
+ full_adder fa3(A[3], B3, C3, S[3], C);
+
 endmodule
 
