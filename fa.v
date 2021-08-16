094890f1139eca1d61455d3158141c7c82bd13b7
amitle2 <73496384+amitle2@users.noreply.github.com>
2021-08-17 00:17:37 +0300

    Update fa.v

--- fa.v
+++ fa.v
@@ -1,12 +1,11 @@
 `timescale 1ns/100ps
 
-
-module full_adder(A, B, Cin, S, Cout);
-
+module FULL_ADDER(A, B, Cin, S, Cout);
+ 
  output S, Cout;
  input A, B, Cin;
-
+ 
  assign S = A ^ B ^ Cin; 
  assign Cout = (A & B) | Cin & (A ^ B);
-
+ 
 endmodule
