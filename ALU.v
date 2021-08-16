4b82660401589193ecbb36c5ec8571f65851156b
amitle2 <73496384+amitle2@users.noreply.github.com>
2021-08-17 00:16:21 +0300

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -7,23 +7,23 @@
 ///  10 A(nor)B
 ///  11 A+B
 
-module Alu(A, B,Op0, Op1, C,V);
-
+ module ALU(A, B,Op0, Op1, C,V);
+ 
  input [3:0] A,B;
  input Op0, Op1;
  output [3:0] C;
  output V;
-
+ 
  wire [3:0] i, i1, i2;
  wire M,Cout;
-
+ 
  assign i1 = ~(A & B);
  assign i2 = ~(A | B);
  assign M = Op1;
-
- FourBitAddSub u1(A, B, M, i, Cout,V);
- mux u2(i1,i2,i,i,Op0,Op1,C);
-
+  
+ ADDER_SUBTRACTOR u1(A, B, M, i, Cout,V);
+ MUX u2(i1,i2,i,i,Op0,Op1,C);
+ 
 endmodule
 
 
